844d716c05909552987e280649ed41f3f79db2a9
Kornel <makusz07@gmail.com>
2018-11-25 20:33:58 +0100

    Add refactor commit to the code.

--- src/main/java/com/myproject/Game.java
+++ src/main/java/com/myproject/Game.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 import java.util.Random;
 
 public class Game implements ActionListener {
+    //pola w klasie powinny mieć modyfikator dostępu - domyślnie teraz jest protected, sugeruję private (poczytajcie o tym)
+    //pola powinny mieć nazwy, które opisują je w taki sposób, że gdy spojrzę to wiem do czego służy dany obiekt czy pole
     JFrame frame = new JFrame("Memorki");
 
     JPanel field = new JPanel();
@@ -19,6 +21,9 @@ public class Game implements ActionListener {
     JPanel start_screen = new JPanel();
     JPanel end_screen = new JPanel();
 
+    //nie tworzy się obiektów w ten sposób, tworzenie obiektów powinno być po kontrolą, ujęte w metody
+    //do usunięcia magic number 20 i magic string Start, Wyjście itp. Najlepiej jest wyciągnąć to do zmiennych statycznych
+    //
     JButton btn[] = new JButton[20];
     JButton start = new JButton("Start");
     JButton over = new JButton("Wyjscie");
@@ -28,12 +33,17 @@ public class Game implements ActionListener {
 
     Random randomGenerator = new Random();
     private boolean purgatory = false;
+    //pole nie jest używane - will be removed in next commit
     JLabel winner;
+    //pole nie jest używane - will be removed in next commit
     Boolean game_over = false;
+    //domyślna wartość int to 0, więc inicjalizacja jest zbędna
     int level=0;
 
 
     String[] board;
+    //pole nie jest używane - will be removed in next commit
+    //formatowanie tekstu, kod powinien wyglądać tak: int[] boardQ = new int[20]; łatwiej się to czyta - will be removed in next commit
     int[] boardQ=new int[20];
     Boolean shown = true;
     int temp=30;
@@ -41,11 +51,13 @@ public class Game implements ActionListener {
     String a[]=new String[10];
     boolean eh=true;
 
+    //magic string
     private JLabel label = new JLabel("Podaj liczbę kart od 1 do 10");
     private JTextField text = new JTextField(10);
 
-
+// bez modyfikatora
     public Game(){
+        //do wyciągnięcia do osobnej metody
         frame.setSize(500,300);
         frame.setLocation(500,300);
         frame.setLayout(new BorderLayout());
@@ -73,12 +85,15 @@ public class Game implements ActionListener {
         frame.add(start_screen, BorderLayout.CENTER);
         frame.setVisible(true);
     }
+    //private access
     public void setUpGame(int x,Boolean what){
         level=x;
         clearMain();
 
+        //magic int
         board = new String[2*x];
         for(int i=0;i<(x*2);i++){
+            //magic string
             btn[i] = new JButton("");
             btn[i].setBackground(new Color(220, 220, 220));
             btn[i].addActionListener(this);
@@ -87,15 +102,18 @@ public class Game implements ActionListener {
 
         }
 
+        //można zrobić z tego enuma
         String[] c = {"auto","dom","chmura","pies","serce","diament","slonce","kot","drzewo","snieszka"};
+        //do osobnej metody
         if(what) a=c;
 
-
+        //magic int
         for(int i=0;i<x;i++){
             for(int z=0;z<2;z++){
                 while(true){
-                    int y = randomGenerator.nextInt(x*2);
-                    if(board[y]==null){
+                    //magic int
+                    int y = randomGenerator.nextInt(x*2); 
+                    if(board[y]==null){ //metoda equals zamiast ==
                         btn[y].setText(a[i]);
                         board[y]=a[i];
                         break;
@@ -108,32 +126,40 @@ public class Game implements ActionListener {
         createBoard();
 
     }
+    //private access
     public void hideField(int x){
+        //magic int i string
         for(int i=0;i<(x*2);i++){
             btn[i].setText("");
         }
         shown=false;
     }
+    //magic string and private access
     public void switchSpot(int i){//this will switch the current spot to either blank or what it should have
+        //stringi porównujemy metodą equals() - !DONE_STRING.equals(board[i])
         if(board[i]!="done"){//when a match is correctly chosen, it will no longer switch when pressed
             if(btn[i].getText()==""){
                 btn[i].setText(board[i]);
 
             } else {
+                //magic string
                 btn[i].setText("");
 
             }
         }
     }
+    //metoda nie jest używana - will be removed in next commit
     public void showSpot(int i){
         btn[i].setText(board[i]);
     }
+    //metoda nie jest używana - will be removed in next commit
     public void showField(int x, String a[]){
-        for(int i=0;i<(x*2);i++){
+        for(int i=0;i<(x*2);i++){//magic int
             btn[i].setText(a[i]);
         }
         shown=true;
     }
+    //metoda nie jest używana - will be removed in next commit
     void waitABit(){
 
         try{
@@ -142,18 +168,20 @@ public class Game implements ActionListener {
 
         }
     }
+    //boolean zwracany przez tę metodę nie jest nigdzie wykorzystywany
     public boolean checkWin(){//checks if every spot is labeled as done
-        for(int i=0;i<(level*2);i++){
-            if (board[i]!="done")return false;
+        for(int i=0;i<(level*2);i++){ //magic int
+            if (board[i]!="done")return false; //magic string, metoda equals zamiast !=
         }
         winner();
         return true;
     }
+    //private access
     public void winner(){
 
         start_screen.remove(field);
         start_screen.add(end_screen, BorderLayout.CENTER);
-        end_screen.add(new TextField("Wygrales"), BorderLayout.NORTH);
+        end_screen.add(new TextField("Wygrales"), BorderLayout.NORTH); //magic string
 
         end_screen.add(goBack);
         goBack.setEnabled(true);
@@ -163,17 +191,20 @@ public class Game implements ActionListener {
 
 
     }
+    //private access
     public void goToMainScreen(){
         new Game();
     }
+    //private access
     public void createBoard(){
         field.setLayout(new BorderLayout());
         start_screen.add(field, BorderLayout.CENTER);
 
-        field.setLayout(new GridLayout(5,4,2,2));
+        field.setLayout(new GridLayout(5,4,2,2)); //można wyrzucić to do zmiennych
         field.setBackground(Color.black);
 
     }
+    //private access
     public void clearMain(){
         start_screen.remove(menu);
         start_screen.remove(menu2);
@@ -182,20 +213,21 @@ public class Game implements ActionListener {
         start_screen.revalidate();
         start_screen.repaint();
     }
+    //metoda na prawie 70 linii, do zmniejszenia/rozbicia na kilka metod
     public void actionPerformed(ActionEvent click){
         Object source = click.getSource();
         if(purgatory){
             switchSpot(temp2);
             switchSpot(temp);
-            temp=(level*2);
-            temp2=30;
-            purgatory=false;
+            temp=(level*2); //magic int
+            temp2=30; //magic int
+            purgatory=false; // magic boolean
         }
         if(source==start){
             try{
                 level = Integer.parseInt(text.getText());
             } catch (Exception e){
-                level=1;
+                level=1; //magic int
             }
 
             setUpGame(level, eh);
@@ -218,19 +250,20 @@ public class Game implements ActionListener {
             easy.setForeground(Color.BLACK);
         }
 
-        for(int i =0;i<(level*2);i++){
+        for(int i =0;i<(level*2);i++){//magic int
             if(source==btn[i]){
                 if(shown){
                     hideField(level);
                 }else{
                     switchSpot(i);
-                    if(temp>=(level*2)){
+                    if(temp>=(level*2)){ //magic int
                         temp=i;
                     } else {
-                        if((board[temp]!=board[i])||(temp==i)){
+                        if((board[temp]!=board[i])||(temp==i)){ //metoda equals zamiast !=
                             temp2=i;
                             purgatory=true;
 
+                            //tutaj jest sporo wolnych linii, dbamy o wygląd i estetykę klasy - will be removed in next commit
 
                         } else {
                             board[i]="done";
@@ -244,7 +277,7 @@ public class Game implements ActionListener {
 
             }
 
-
+//ta sama uwaga ze zbędnymi liniami - will be removed in next commit
         }
 
 
